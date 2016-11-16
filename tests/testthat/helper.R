f3e48cc61efbaccc704f79c1d1449bf49f7393ac
Jim Hester <james.f.hester@gmail.com>
2016-11-16 15:58:46 -0500

    Cache package installs to speed things up greatly

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,15 +1,15 @@
 with_package <- function(f, ...) {
-  function(name, code) {
-    unloadNamespace(name)
-    lib <- tempfile()
-    on.exit(unlink(lib, recursive = TRUE), add = TRUE)
+  memoise::memoise(function(name, code) {
+    tryCatch(unloadNamespace(name), error = function(e) NULL)
+    lib <- tempfile(tmpdir = ".cache")
+    #on.exit(unlink(lib, recursive = TRUE), add = TRUE)
     dir.create(lib)
     libpath <- .libPaths()
     on.exit(.libPaths(libpath), add = TRUE)
     .libPaths(c(lib, .libPaths()))
     f(name, ...)
     force(code)
-  }
+  }, cache = memoise::cache_filesystem(".cache"))
 }
 with_cran_package <- with_package(function(...) {
   old <- options(repos = c(CRAN = "https://cloud.r-project.org"))
@@ -20,3 +20,9 @@ with_cran_package <- with_package(function(...) {
 with_local_package <- with_package(devtools::install, repo = NULL, type = "source", quiet = TRUE)
 
 with_github_package <- with_package(devtools::install_github, quiet = TRUE)
+
+# Counter is used if server state has changed (PUT, POST etc)
+.counter <- 0
+gh <<- memoise::memoise(environment(gh)[["_f"]], cache = memoise::cache_filesystem(".cache"), ~ .counter)
+r_github_content <<- memoise::memoise(environment(r_github_content)[["_f"]], cache = memoise::cache_filesystem(".cache"), ~ .counter)
+package_github_content <<- memoise::memoise(environment(package_github_content)[["_f"]], cache = memoise::cache_filesystem(".cache"), ~ .counter)
