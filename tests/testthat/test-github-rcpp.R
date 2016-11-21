79134cffc43da3aeba23ba597b0f6871aad1cf8f
Jim Hester <james.f.hester@gmail.com>
2016-11-21 11:42:23 -0500

    Fix tests for current behavior

--- tests/testthat/test-github-rcpp.R
+++ tests/testthat/test-github-rcpp.R
@@ -1,18 +1,37 @@
 context("github-rcpp")
-
 with_github_package("tidyverse/tibble", {
-  test_that("lookup_github_cran", {
-    skip_on_cran()
-
-    res <- lookup_rcpp_github("matrixToDataFrame", "tidyverse", "tibble")
-    expect_equal(length(res), 1L)
-    expect_match(res[[1]]$content, "List matrixToDataFrame")
+  test_that("fetch_symbol_map", {
+    s <- as.source_type("tibble", "rcpp")
+    s <- fetch_symbol_map(s)
+    expect_true(length(s$map_lines) > 0)
   })
 
-  test_that("lookup_rcpp", {
-    res <- lookup_rcpp("matrixToDataFrame", "tibble")
+  test_that("parse_symbol_map", {
+    s <- as.source_type("tibble", "rcpp")
+    s <- fetch_symbol_map(s)
 
-    expect_equal(length(res), 1L)
-    expect_match(res[[1]]$content, "List matrixToDataFrame")
+    s <- parse_symbol_map(s)
+    expect_true(length(s$map) > 0)
   })
 })
+  #test_that("lookup_github_cran", {
+    #skip_on_cran()
+
+    #res <- lookup_rcpp_github("matrixToDataFrame", "tidyverse", "tibble")
+    #expect_equal(length(res), 1L)
+    #expect_match(res[[1]]$content, "List matrixToDataFrame")
+  #})
+
+  #test_that("lookup_rcpp", {
+    #res <- lookup_rcpp("matrixToDataFrame", "tibble")
+
+    #expect_equal(length(res), 1L)
+    #expect_match(res[[1]]$content, "List matrixToDataFrame")
+  #})
+
+  #test_that("lookup_rcpp", {
+    #res <- lookup_rcpp("matrixToDataFrame2", "tibble")
+
+    #expect_identical(length(res), NULL)
+  #})
+#})
--- tests/testthat/test-local-rcpp.R
+++ tests/testthat/test-local-rcpp.R
@@ -2,47 +2,17 @@ with_local_package("TestRcpp", {
   context("local-rcpp")
   test_that("fetch_symbol_map", {
     s <- as.source_type("TestRcpp", "rcpp")
-    map <- fetch_symbol_map(s)
-    expect_true(length(map) > 1)
+    s <- fetch_symbol_map(s)
+    expect_true(length(s$map_lines) > 1)
 
     expect_error(as.source_type("missing", "rcpp"), "no package 'missing' was found")
   })
 
-  test_that("fetch_symbol_map", {
-    s <- as.source_type("TestRcpp", "rcpp")
-    map <- fetch_symbol_map(s)
-    expect_true(length(map) > 1)
-  })
   test_that("parse_symbol_map", {
     s <- as.source_type("TestRcpp", "rcpp")
     s <- fetch_symbol_map(s)
 
-    s <- parse_symbol_map(map)
-    expect_true(length(map) > 1)
+    s <- parse_symbol_map(s)
+    expect_true(length(s$map) > 1)
   })
 })
-
-    #map <- rcpp_symbol_map_local("TestRcpp")
-
-    #expect_true("add_rcpp" %in% names(map))
-
-    #expect_true(grepl(map[["add_rcpp"]], "double add_rcpp(double x, double y)"))
-
-    #expect_true(grepl(map[["add_rcpp_default"]], "double add_rcpp_default(double x, double y = 1)"))
-  #})
-
-  #test_that("lookup_rcpp_local", {
-    #res <- lookup_rcpp_local("add_rcpp", "TestRcpp")
-    #expect_equal(length(res), 1L)
-
-    #expect_equal(res[[1]]$content, "double add_rcpp(double x, double y) {\n  return x + y;\n}")
-  #})
-
-  #test_that("lookup_rcpp", {
-    #res <- lookup_rcpp("add_rcpp", "TestRcpp")
-    #expect_equal(length(res), 1L)
-
-    #expect_equal(res[[1]]$content, "double add_rcpp(double x, double y) {\n  return x + y;\n}")
-  #})
-
-#})
