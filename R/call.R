a962a2961d195d7267b3b33846e1d028c52f21f7
Jim Hester <james.f.hester@gmail.com>
2016-11-29 17:10:32 -0500

    Support for CRAN packages using .Call

--- R/call.R
+++ R/call.R
@@ -1,6 +1,11 @@
 #' @include rcpp.R
 parse_symbol_map.call <- function(s) {
-  s$map <- s$map_lines$nativeRoutines[[1]]$symbolNames
+  if (length(s$map_lines$nativeRoutines) == 0) {
+    s$map <- setNames(s$search, s$search)
+  } else {
+    s$map <- s$map_lines$nativeRoutines[[1]]$symbolNames
+  }
+
   s$regex <- s$map[s$search]
   s
 }
@@ -19,6 +24,16 @@ fetch_symbol_map.call_local <- function(s, ...) {
   s
 }
 
+# Helper function to write the remote namespace file to a temporary directory
+# as parseNamespaceFile only operates on files, not contents
+fetch_remote_namespace <- function(content) {
+  temp_dir <- tempfile()
+  dir.create(temp_dir)
+  temp_file <- file.path(temp_dir, "NAMESPACE")
+  writeLines(content, con = temp_file)
+  parseNamespaceFile(basename(temp_dir), dirname(temp_dir))
+}
+
 fetch_symbol_map.call_github <- function(s) {
   res <- github_content(
     path = paths(s$description$RemoteSubdir, "NAMESPACE"),
@@ -26,11 +41,18 @@ fetch_symbol_map.call_github <- function(s) {
     repo = s$description$RemoteRepo,
     ref = s$description$RemoteRef,
     api_url = s$descripiton$RemoteHost)
-  temp_dir <- tempfile()
-  dir.create(temp_dir)
-  temp_file <- file.path(temp_dir, "NAMESPACE")
-  writeLines(res, con = temp_file)
-  s$map_lines <- parseNamespaceFile(basename(temp_dir), dirname(temp_dir))
+  s$map_lines <- fetch_remote_namespace(res)
+  s
+}
+
+fetch_symbol_map.call_cran <- function(s) {
+  res <- github_content(
+    path = "NAMESPACE",
+    owner = "cran",
+    repo = s$description$Package,
+    ref = s$description$Version)
+
+  s$map_lines <- fetch_remote_namespace(res)
   s
 }
 
@@ -54,6 +76,17 @@ source_files.call_github <- function(s, name = s$search, ...) {
     repo = s$description$RemoteRepo,
     language = "c",
     api_url = s$description$RemoteHost)
+
+  s
+}
+
+source_files.call_cran <- function(s, name = s$search, ...) {
+  s$src_files <- github_code_search(
+    name = name,
+    path = "src/",
+    owner = "cran",
+    repo = s$description$Package,
+    language = "c")
 
   s
 }
---
+++ tests/testthat/test-cran-call.R
@@ -0,0 +1,49 @@
+context("cran-call")
+
+# fasttime is a very small package with no dependencies that uses .Call
+with_cran_package("fasttime", {
+  s <- NULL
+  test_that("fetch_symbol_map", {
+    s <<- as.source_type("fasttime", "call", "parse_ts")
+    expect_equal(class(s), c("call_cran", "call", "cran"))
+
+    s <<- fetch_symbol_map(s)
+    expect_true(length(s$map_lines) > 1)
+
+    expect_error(as.source_type("missing", "call"), "no package 'missing' was found")
+  })
+
+  test_that("parse_symbol_map", {
+    s <<- parse_symbol_map(s)
+    expect_true(length(s$map) > 0)
+  })
+
+  test_that("source_files", {
+    s <<- source_files(s)
+    expect_equal(basename(s$src_files), "tparse.c")
+  })
+
+  test_that("fetch_source", {
+    s <<- fetch_source(s, s$src_files[[1]])
+    expect_true(length(s$src_lines) > 1)
+  })
+
+  test_that("parse_source", {
+    s <<- parse_source(s, s$map[s$search])
+    expect_true(length(s$fun_lines) > 1)
+
+    expect_true(s$fun_start > 1)
+    expect_true(s$fun_end > s$fun_start)
+  })
+
+  test_that("lookup_function", {
+    res <- lookup_function("parse_ts", "call", "fasttime")
+
+    expect_true(nchar(res$content) > 0)
+    expect_equal(res$remote_type, "cran")
+    expect_equal(res$type, "call")
+    expect_equal(res$language, "c")
+
+    expect_null(lookup_function("missing", "call", "fasttime"))
+  })
+})
