f8d7d7af2e0c51ac5ff8468f66963df711f722d6
Metin Yazici <stradivariusboul@gmail.com>
2019-05-14 21:23:17 +0200

    Add user and language arguments to `lookup_usage()`

--- R/github.R
+++ R/github.R
@@ -19,16 +19,39 @@ github_code_search <- function(name, path = "src/", owner, repo, language = c("c
 
 #' Search for code usage on CRAN
 #'
-#' This function uses GitHub's code search to search for a code usage in all
-#' packages on CRAN. It will open a browser window with the results so they can
-#' be explored.
+#' This function uses GitHub's code search to search for a code usage. As default the
+#' term will be searched in all packages on CRAN. It will open a browser window with
+#' the results so they can be explored.
 #' @param x The term to search
+#' @param user The user. Default is cran.
+#' @param language The language the term written by. Default value is R.
 #' @export
 #' @examples
 #' \dontrun{
 #' lookup_usage("grep")
+#' lookup_usage("length", language = "C++")
+#' lookup_usage("R_NamesSymbol", user = NULL, language = "C")
 #' }
-lookup_usage <- function(x) {
-  url <- paste("https://github.com/search?l=r&q=%22", as.character(x), "%22+user%3Acran+language%3AR&ref=searchresults&type=Code&utf8=%E2%9C%93", sep = "", collapse = "")
+lookup_usage <- function(x, user = "cran", language = "R") {
+  user_detail <- if (!(is.null(user) || is.na(user))) {
+    paste0("+user%3A", URLencode(as.character(user), reserved = TRUE))
+  } else {
+    NULL
+  }
+  language_detail <- if (!(is.null(language) || is.na(language))) {
+    paste0("+language%3A", URLencode(as.character(language), reserved = TRUE))
+  } else {
+    NULL
+  }
+  url <- paste(
+    "https://github.com/search?l=&q=",
+    URLencode(as.character(x)),
+    user_detail,
+    language_detail,
+    "&ref=searchresults&type=Code&utf8=%E2%9C%93",
+    sep = "",
+    collapse = ""
+  )
   browseURL(url)
 }
+
